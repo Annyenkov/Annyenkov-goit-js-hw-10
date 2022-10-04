e0978cdd72c1716a48e3272f4aaf69b30620f617
Annyenkov <annyenkov@gmail.com>
2022-10-04 02:43:57 +0000

    Deploying to gh-pages from @ Annyenkov/goit-js-hw-10@245bd6c71ca1a13b28ebcfbfa3429a95d35dc0ae 🚀

---
+++ index.82a25765.js
@@ -0,0 +1,2 @@
+!function(){var o=document.querySelector(body);console.dir(o)}();
+//# sourceMappingURL=index.82a25765.js.map
---
+++ index.faefdf96.js
@@ -0,0 +1,2 @@
+const o=document.querySelector(body);console.dir(o);
+//# sourceMappingURL=index.faefdf96.js.map
